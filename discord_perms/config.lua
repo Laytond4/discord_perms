a53e98df7a49eaf0cd7a35f4ae0e0ab7a2a291dd
Laytond4 <68480674+Laytond4@users.noreply.github.com>
2020-07-18 16:06:45 -0400

    Update config.lua

--- discord_perms/config.lua
+++ discord_perms/config.lua
@@ -1,6 +1,6 @@
 Config = {
-	DiscordToken = "Bot Discord Token Here",
-	GuildId = "Guild ID Here",
+	DiscordToken = "NzM0MTI5NzgzMjM5MzQ0MjE4.XxNNwg.snPN01TYbQzaZV1HvU1B7Vs01Mo",
+	GuildId = "714596721396809931",
 
 	-- Format: ["Role Nickname"] = "Role ID" You can get role id by doing \@RoleName
 	Roles = {
